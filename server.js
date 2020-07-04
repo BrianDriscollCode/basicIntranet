fc3229a7d52ca6159799569efedab18fc5d8f338
BrianDriscollCode <bdriscoll407@gmail.com>
2020-07-04 14:05:47 -0700

    Create top navigation bar and place positions for side and main content but still need to work on exact positioning.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 app.use(express.static('./public/styling'));
+app.use(express.static('./public/users'));
 
 app.listen(3000);
 
@@ -12,8 +13,4 @@ console.log('server activated');
 
 app.get('/', (req, res) => {
   res.render('home');
-});
-
-app.get('/header', (req, res) => {
-  res.render('./universalDesigns/header');
 });
